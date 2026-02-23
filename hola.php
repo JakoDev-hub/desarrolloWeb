693bb2a4f2369a97eaececd7473e7f3bd8ee087a
jako <jakothedev@gmail.com>
2026-02-23 13:55:57 -0300

    inicioPHP

---
+++ hola.php
@@ -0,0 +1,19 @@
+<?php
+echo "hola mundo";
+echo $_SERVER ['HTTP_USER_AGENT'];
+if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Edg')) {
+    echo 'Está utilizando edge.';
+
+?>
+    <h3>str_contains() ha devuelto true</h3>
+<?php
+} else {
+}
+    ?>
+    <p>esto no es edge</p>
+    <?php
+
+
+?>
+
+
