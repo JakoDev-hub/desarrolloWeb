3ab59338469565fab7097b426e0e336314b621ed
jako <jakothedev@gmail.com>
2026-02-24 16:58:39 -0300

    continuopruebas1

--- hola.php
+++ hola.php
@@ -1,4 +1,5 @@
 <?php
+
 echo "hola mundo";
 echo $_SERVER ['HTTP_USER_AGENT'];
 if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Edg')) {
@@ -8,12 +9,12 @@ if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Edg')) {
     <h3>str_contains() ha devuelto true</h3>
 <?php
 } else {
+    
 }
     ?>
     <p>esto no es edge</p>
     <?php
-
-
+    //son lo mismo pero con diferente sintaxis
+    echo "hola mundo"; 
 ?>
-
-
+    
